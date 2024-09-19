e64621f704ff9a3dd1acf40094764efa22674f6e
Aretemii Korshunov <azatotgame@gmail.com>
2024-09-19 13:17:16 +0300

    fixed programm

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,21 +5,7 @@
         public static void Main(string[] args)
         {
             //Koodi tähän
-            Console.Write("Syötä ikäsi: ");
-            int ika = int.Parse(Console.ReadLine());
-
-            if (ika < 10)
-            {
-                Console.WriteLine("    olet lapsi.");
-            }
-            else if (ika < 18)
-            {
-                Console.WriteLine(" olet teini.");
-            }
-            else
-            {
-                Console.WriteLine("olet aikuinen.   ");
-            }
+          
         }
 
     }
