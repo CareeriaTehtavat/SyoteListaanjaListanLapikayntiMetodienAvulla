7e5bcab295d22b97fb153044abf4c09b3dac1630
Aretemii Korshunov <azatotgame@gmail.com>
2024-09-19 10:19:06 +0300

    versio 1

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,14 +5,7 @@
         public static void Main(string[] args)
         {
             //Koodi tähän
-            int luku1, luku2;
 
-            Console.Write("Anna eka luku: ");
-            luku1 = int.Parse(Console.ReadLine());
-            Console.Write("Anna toka luku: ");
-            luku2 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("\nSyöttämäsi luvut: " + luku2 + " ja " + luku1);
 
         }
 
