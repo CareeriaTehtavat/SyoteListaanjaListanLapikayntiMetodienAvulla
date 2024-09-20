3bf70e661f78193c2a64bace144af37115663f66
Aretemii Korshunov <azatotgame@gmail.com>
2024-09-20 13:12:14 +0300

    fixed programm

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,19 +5,10 @@
         public static void Main(string[] args)
         {
             //Koodi tähän
-            int luku;
-
-            Console.Write("Anna luku: ");
-            luku = int.Parse(Console.ReadLine());
-            Console.WriteLine();
-            Console.WriteLine("Kertitaulu luvulle " + luku + ":");
-            for (int i = 0; i < 10; i++) {
-                Console.WriteLine(luku + " * " +(i+1) + " = " + (luku * (i+1)));
-            }
-
+          
 
         }
 
     }
-    }
+}
 
